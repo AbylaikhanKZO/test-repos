59656fbd968b7e3f5d8efa9c5465099e8b0808e4
Your Name <you@example.com>
2022-10-18 21:14:39 +0600

    task with folders done

---
+++ script.js
@@ -0,0 +1,38 @@
+let arr= [   
+     {  
+     name: "Folder 1",
+     folders: [
+          {name: "Subfolder 1"},
+          {name: "Subfolder 2"},
+          {name: "Subfolder 3"},
+     ]
+},
+{     
+     name: "folder 2",
+     folders: [
+          {name: "Subfolder 4"},
+          {name: "Subfolder 5"},
+          {name: "Subfolder 6"},
+     ]
+}, 
+{
+     name: "folder 3",
+     folders: [
+     {name: "Subfolder 7"},
+     {name: "Subfolder 8"},
+     {name: "Subfolder 9"},
+          ]
+     },
+]
+
+     function showParentFolderName(SubfolderName) {
+      for(let folder of arr) {
+          for(let Subfolder of folder.folders){
+               if(Subfolder.name === SubfolderName){
+                    console.log(folder.name)
+               break}
+          }
+          }    s
+     }
+         
+showParentFolderName("Subfolder 2")
